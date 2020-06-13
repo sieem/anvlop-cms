33857eb468ac5fba44e96b482c9e14f17eb0cb7a
Siem Lasseel <siem.linx@gmail.com>
2020-06-13 13:50:41 +0200

    Add admin routing

--- libs/admin/projects/src/lib/admin-projects.module.ts
+++ libs/admin/projects/src/lib/admin-projects.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
 
-export const adminProjectsRoutes: Route[] = [];
+export const adminProjectsRoutes: Route[] = [
+  {
+    path: '',
+    component: OverviewComponent
+  }
+];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [OverviewComponent],
+  exports: [OverviewComponent],
 })
 export class AdminProjectsModule {}
