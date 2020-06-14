f2bae676c29a47fcac91aa623bb8f323c3586036
Siem Lasseel <siem.linx@gmail.com>
2020-06-14 09:56:20 +0200

    User crud functions

--- libs/api/user/src/lib/user.controller.ts
+++ libs/api/user/src/lib/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpStatus, HttpException, Param, Put, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -7,17 +7,44 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UserController {
     constructor(private readonly UserService: UserService) { }
 
+    @Get('users')
+    async findAll(): Promise<User[]> {
+        return this.UserService.findAll();
+    }
+
+    @Get('user/:id')
+    async findOneById(@Param() params: any): Promise<User> {
+        try {
+            return await this.UserService.findById(params.id);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.NOT_FOUND);
+        }
+    }
+
     @Post('user')
     async create(@Body() createUserDto: CreateUserDto) {
         try {
-            await this.UserService.create(createUserDto);
+            return await this.UserService.create(createUserDto);
         } catch (error) {
             throw new HttpException(error, HttpStatus.BAD_REQUEST);
         }
     }
 
-    @Get('users')
-    async findAll(): Promise<User[]> {
-        return this.UserService.findAll();
+    @Put('user/:id')
+    async update(@Body() updatedValues: User, @Param() params: any) {
+        try {
+            return await this.UserService.update(updatedValues, params.id);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Delete('user/:id')
+    async delete(@Param() params: any) {
+        try {
+            return await this.UserService.delete(params.id);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- libs/api/user/src/lib/user.service.ts
+++ libs/api/user/src/lib/user.service.ts
@@ -8,12 +8,35 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UserService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
+    async findAll(): Promise<User[]> {
+        return this.userModel.find().exec();
+    }
+
+    async findById(id: string): Promise<User> {
+        return await this.userModel.findById(id).exec();
+    }
+
     async create(createUserDto: CreateUserDto): Promise<User> {
         const createdUser = new this.userModel(createUserDto);
         return await createdUser.save();
     }
 
-    async findAll(): Promise<User[]> {
-        return this.userModel.find().exec();
+    async update(updatedValues: User, id: string): Promise<User> {
+        const oldUser = await this.findById(id);
+        const updatedUser = oldUser;
+
+        for (const key in updatedValues) {
+            if (!updatedValues.hasOwnProperty(key)) {
+                continue;
+            }
+            updatedUser[key] = updatedValues[key];
+        }
+
+        return await updatedUser.save();
+    }
+
+    async delete(id: string) {
+        const user = await this.findById(id);
+        return await user.remove();
     }
 }
