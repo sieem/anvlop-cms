770e492ae946908978387ceba488ced6fa77e818
Siem Lasseel <siem.linx@gmail.com>
2020-07-12 15:19:47 +0200

    Vercel deploy

--- libs/api/asset/src/lib/asset.service.ts
+++ libs/api/asset/src/lib/asset.service.ts
@@ -6,7 +6,11 @@ import { Project, IAsset } from '@anvlop/shared/interfaces';
 @Injectable()
 export class AssetService {
     fireBaseInit = admin.initializeApp({
-        credential: admin.credential.cert('./firebase.json'),
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+        }),
         storageBucket: 'anvlop-cms.appspot.com'
     });
     bucket = admin.storage().bucket();
