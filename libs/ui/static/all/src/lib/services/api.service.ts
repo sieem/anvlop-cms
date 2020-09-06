54a9826eceb78548ab5c50a758f466e4fa00a8e8
Siem Lasseel <siem.linx@gmail.com>
2020-09-06 12:37:25 +0200

    Share app environment file to libs

--- libs/ui/static/all/src/lib/services/api.service.ts
+++ libs/ui/static/all/src/lib/services/api.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { isScullyGenerated, TransferStateService } from '@scullyio/ng-lib';
 import { tap } from 'rxjs/operators';
+import { APP_CONFIG } from '@anvlop/shared/config';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,13 @@ export class ApiService {
   constructor(
     private http: HttpClient,
     private readonly transferStateService: TransferStateService,
+    @Inject(APP_CONFIG) private appConfig: any,
   ) { }
 
   public httpCall(url, key) {
     return isScullyGenerated()
       ? this.transferStateService.getState<any>(key)
-      : this.http.get<any>(url).pipe(
+      : this.http.get<any>(this.appConfig.apiUrl + '/' + url).pipe(
         tap(item => this.transferStateService.setState<any>(key, item)));
   }
 }
