c35c9b4bf8c194eacf0619e1427be76e5af8e053
Siem Lasseel <siem.linx@gmail.com>
2020-09-13 10:29:29 +0200

    Adding custom css

--- libs/ui/static/all/src/lib/components/app.component.ts
+++ libs/ui/static/all/src/lib/components/app.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsStoreService } from '../settings-store.service';
 
 @Component({
   selector: 'anvlop-app',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  public customCss$ = this.settingsStoreService.getSettingsValue('customCss');
 
-  constructor() { }
+  constructor(
+    private settingsStoreService: SettingsStoreService,
+  ) { }
 
   ngOnInit(): void {
+    this.injectCustomCss();
+  }
+
+  async injectCustomCss(): Promise<void> {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.type = 'text/css';
+    linkElement.href = `data:text/css;base64,${btoa(await this.customCss$)}`;
+    document.querySelector('head').appendChild(linkElement);
   }
 
 }
