9a70e733f4f6a6a913f3cc8b66d150a83cafee88
Siem Lasseel <siem.linx@gmail.com>
2020-09-15 19:28:25 +0200

    ui-admin: sortable files

--- libs/ui/shared/src/lib/ui-shared.module.ts
+++ libs/ui/shared/src/lib/ui-shared.module.ts
@@ -6,10 +6,11 @@ import { SlugDirective } from './directives/slug.directive';
 import { MainAssetPipe } from './main-asset.pipe';
 import { NotMainAssetsPipe } from './not-main-assets.pipe';
 import { SafeHtmlPipe } from './pipes/safe-html.pipe';
+import { FileNamePipe } from './pipes/file-name.pipe';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
-  declarations: [FileUrlPipe, SlugDirective, MainAssetPipe, NotMainAssetsPipe, SafeHtmlPipe],
-  exports: [FileUrlPipe, SlugDirective, MainAssetPipe, NotMainAssetsPipe, SafeHtmlPipe],
+  declarations: [FileUrlPipe, SlugDirective, MainAssetPipe, NotMainAssetsPipe, SafeHtmlPipe, FileNamePipe],
+  exports: [FileUrlPipe, SlugDirective, MainAssetPipe, NotMainAssetsPipe, SafeHtmlPipe, FileNamePipe],
 })
 export class UiSharedModule {}
